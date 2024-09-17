0b4bf3c0c4fd75cd033fd79f52e01d23fa65f92d
rizkyhafidz929@gmail.com <rizkyhafidz929@gmail.com>
2024-09-17 10:44:00 +0700

    tambah dei

--- create.php
+++ create.php
@@ -1,2 +1,14 @@
 <?php 
-Str::createUuidsNormally()?>
+Str::createUuidsNormally()
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
